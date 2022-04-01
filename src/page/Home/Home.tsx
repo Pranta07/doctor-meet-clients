10d438b2663c56e12fecd1c2c8e8464ca1e98df3
mhasancy <mhasancy@gmail.com>
2022-04-01 19:20:04 +0600

    added nav

--- src/page/Home/Home.tsx
+++ src/page/Home/Home.tsx
@@ -3,93 +3,52 @@ import Articles from "../../components/Articles/Articles";
 // import Banner from "../../components/Banner/Banner";
 import HealthCareProvider from "../../components/HealthCareProvider/HealthCareProvider";
 import Just_Banner from "../../components/just-banner/Just_Banner";
+import Nav from "../../components/Nav/Nav";
 import Review from "../../components/Review/Review";
-
 // import Login from "../../components/security/Login/Login";
 // import Registation from "../../components/security/Registation/Registation";
 import Services from "../../components/Services/Services";
 
 const Home = () => {
-    return (
-        <>
-            {/* alamin */}
-            {/* <Banner></Banner> */}
-            {/* mahadi */}
-            
-            <Just_Banner></Just_Banner>
+  return (
+    <>
+      {/* alamin */}
+      {/* <Banner></Banner> */}
+      {/* mahadi */}
+      <Nav />
+      <Just_Banner></Just_Banner>
 
+      {/* mahadi */}
+      {/* <Login></Login> */}
+      <Services></Services>
+      {/* PRANTHA */}
 
+      {/* PRANTHA */}
+      {/* ridoy */}
 
+      <HealthCareProvider></HealthCareProvider>
 
-            {/* mahadi */}
-            {/* <Login></Login> */}
-            <Services></Services>
-            {/* PRANTHA */}
+      {/* ridoy */}
 
+      {/* mahim */}
+      {/* <Registation></Registation> */}
 
+      <Review></Review>
 
+      {/* mahim */}
 
+      {/* mostofa */}
+      <Articles></Articles>
 
+      {/* ridoy */}
+      {/* mostofa */}
 
+      {/* mostofa */}
+      {/* ridoy */}
 
-            {/* PRANTHA */}
-            {/* ridoy */}
-
-
-
-
-
-
-            <HealthCareProvider></HealthCareProvider>
-
-
-
-            {/* ridoy */}
-
-            {/* mahim */}
-            {/* <Registation></Registation> */}
-
-
-
-
-             <Review></Review>
-
-
-            {/* mahim */}
-            
-            {/* mostofa */}
-            <Articles></Articles>
-
-
-
-
-
-
-
-
-
-
-
-            {/* ridoy */}
-            {/* mostofa */}
-
-
-            {/* mostofa */}
-            {/* ridoy */}
-
-
-
-
-
-
-
-
-
-            {/* ridoy */}
-
-
-        </>
-    );
+      {/* ridoy */}
+    </>
+  );
 };
 
 export default Home;
