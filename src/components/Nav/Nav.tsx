48bd800ad2ea5f3c90f768ed13bf93dc3eac3c7b
mhasancy <mhasancy@gmail.com>
2022-04-01 23:21:35 +0600

    nav updated

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,13 +1,14 @@
 import "bootstrap";
 import React, { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 // import "./Nav.css";
 // import { HashLink } from "react-router-hash-link";
 const Nav = () => {
   const [offset, setOffset] = useState(0);
   const parallaxNav =
-    offset < 100
-      ? "navbar navbar-b navbar-expand-md fixed-top"
-      : "navbar navbar-b navbar-light bg-light navbar-expand-md fixed-top";
+    offset < 50
+      ? "navbar navbar-expand-lg navbar-light fw-bold"
+      : "sticky-top navbar navbar-expand-lg shadow navbar-light bg-light fw-bold";
   useEffect(() => {
     window.onscroll = () => {
       setOffset(window.pageYOffset);
@@ -16,9 +17,57 @@ const Nav = () => {
 
   return (
     <>
-      {" "}
       <nav className={parallaxNav}>
-        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top"> */}
+        <div className="container">
+          <NavLink className="navbar-brand fs-2 ps-md-5 ms-md-5" to="/">
+            <span>Doctors Meet</span>
+          </NavLink>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <NavLink aria-current="page" className="nav-link active" to="/">
+                  Home
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/review-booking/:serviceId">
+                  Find a doctor
+                </NavLink>
+              </li>
+
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/my-bookings">
+                  Apps
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/adding-tour-plan">
+                  Testimonials
+                </NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/manage-bookings">
+                  About us
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      {/* {" "}
+      <nav className={parallaxNav}>
+        {/* <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top"> *
         <div className="container-fluid">
           <a className="navbar-brand" href="/">
             Navbar
@@ -36,70 +85,35 @@ const Nav = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
-              <a className="nav-link" href="/">
-                Features
-              </a>
-              <a className="nav-link" href="/">
-                Pricing
-              </a>
-              <a className="nav-link disabled" href="/" aria-disabled="true">
-                Disabled
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
-      {/* <nav className={parallaxNav}>
-        <div className="container">
-          <NavLink className="navbar-brand" to="/">
-            <a className="navbar-brand js-scroll" href="/">
-              <img src="logo.png" alt="" />{" "}
-              <span style={{ fontWeight: "700" }}>Doctors Meet Up</span>
-            </a>
-          </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarTogglerDemo02"
-            aria-controls="navbarTogglerDemo02"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
-            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <NavLink aria-current="page" className="nav-link" to="/#home">
-                  Home
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/#about">
-                  About
-                </NavLink>
-              </li>
+              <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li className="nav-item">
+                  <NavLink aria-current="page" className="nav-link" to="/#home">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/#about">
+                    About
+                  </NavLink>
+                </li>
 
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/#projects">
-                  Projects
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/#contact">
-                  Contact
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className="nav-link" to="/blogs">
-                  Blogs
-                </NavLink>
-              </li>
-            </ul>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/#projects">
+                    Projects
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/#contact">
+                    Contact
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/blogs">
+                    Blogs
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav> */}
