4e78054d8a90cd24f94fab3c784d83f0f413732f
Al Amin <alaminhossain2000and@gmail.com>
2022-04-01 10:16:06 +0600

    file creation for videochat

---
+++ src/components/VideoChatClient/VideoChatClient.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VideoChatClient = () => {
+    return (
+        <div>
+            <h1>Hello from VideoChatClient</h1>
+        </div>
+    );
+};
+
+export default VideoChatClient;
