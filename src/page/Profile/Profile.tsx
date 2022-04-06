b197b32e1ff455deb2442a56c22c5b0a7c00e703
mahim-shariar <mdmahim924214@gmail.co>
2022-04-06 22:04:22 +0600

    tailwind is delete

---
+++ src/page/Profile/Profile.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Profile.css'
+
+const Profile = () => {
+    return (
+        <div>
+            <h1> 
+                this is profile
+            </h1>
+        </div>
+    );
+};
+
+export default Profile;
