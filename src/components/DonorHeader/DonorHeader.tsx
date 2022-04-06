f01e875569fb324e287d466dae2f5c7c9ee05575
Pranta07 <palpranta@gmail.com>
2022-04-07 02:52:19 +0600

    header done

--- src/components/DonorHeader/DonorHeader.tsx
+++ src/components/DonorHeader/DonorHeader.tsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import vector1 from "../../Assets/blood donation/vector1.jpg";
 
 const DonorHeader = () => {
-    return <div>header</div>;
+    return (
+        <>
+            <section className="container">
+                <div
+                    className="
+                row
+                justify-content-center
+                align-items-center
+                blood-donate
+            "
+                >
+                    <div className="section-intro col-10 col-md-6 col-lg-6">
+                        <h1 className="fw-bold">Donate Blood & Save Life</h1>
+                        <p className="text-secondary">
+                            <small>
+                                Roll up your sleeves and contribute proactively
+                                in this noble cause. A drop of blood can save a
+                                life! Donate blood and get real blessings. Your
+                                droplets of blood may create an ocean of
+                                happiness.
+                            </small>
+                        </p>
+                        <button className="button btn btn-outline-primary mb-2 rounded-pill">
+                            Join Us <FontAwesomeIcon icon={faArrowRight} />
+                        </button>
+                    </div>
+                    <div className="col-10 col-md-6 col-lg-6">
+                        <img src={vector1} alt="" className="w-100" />
+                    </div>
+                </div>
+            </section>
+        </>
+    );
 };
 
 export default DonorHeader;
