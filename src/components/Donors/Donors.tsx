863dada33c386f9d122f2130503099ec3e7efbcc
Pranta07 <palpranta@gmail.com>
2022-04-08 01:35:57 +0600

    code change

--- src/components/Donors/Donors.tsx
+++ src/components/Donors/Donors.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Container } from "react-bootstrap";
