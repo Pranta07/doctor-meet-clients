35266ca418ea813b7264c51f37f3cdee8ebb3e7d
Pranta07 <palpranta@gmail.com>
2022-04-07 17:50:41 +0600

    ui updated

--- src/components/DonorFilter/DonorFilter.tsx
+++ src/components/DonorFilter/DonorFilter.tsx
@@ -20,49 +20,53 @@ const DonorFilter = () => {
                         rounded
                     "
                 />
-                <div className="row justify-content-center gy-2">
-                    <div className="col-10 col-md-4">
-                        <h5 className="text-danger fw-bold">Blood group:</h5>
-                        <select
-                            className="form-select"
-                            aria-label="Default select example"
-                        >
-                            <option selected>Select Blood Group</option>
-                            <option value="1">A+</option>
-                            <option value="2">A-</option>
-                            <option value="3">B+</option>
-                            <option value="3">B-</option>
-                            <option value="3">O+</option>
-                            <option value="3">O-</option>
-                            <option value="3">AB+</option>
-                            <option value="3">AB-</option>
-                        </select>
+                <form>
+                    <div className="row justify-content-center gy-2">
+                        <div className="col-10 col-md-4">
+                            <h5 className="text-danger fw-bold">
+                                Blood group:
+                            </h5>
+                            <select
+                                className="form-select"
+                                aria-label="Default select example"
+                            >
+                                <option selected>Select Blood Group</option>
+                                <option value="1">A+</option>
+                                <option value="2">A-</option>
+                                <option value="3">B+</option>
+                                <option value="3">B-</option>
+                                <option value="3">O+</option>
+                                <option value="3">O-</option>
+                                <option value="3">AB+</option>
+                                <option value="3">AB-</option>
+                            </select>
+                        </div>
+                        <div className="col-10 col-md-4">
+                            <h5 className="text-danger fw-bold">District:</h5>
+                            <select
+                                className="form-select"
+                                aria-label="Default select example"
+                            >
+                                <option selected>Select District</option>
+                                <option value="1">Dhaka</option>
+                                <option value="2">Chittagong</option>
+                                <option value="3">Comilla</option>
+                                <option value="3">Rajshahi</option>
+                                <option value="3">Khulna</option>
+                                <option value="3">Jessore</option>
+                                <option value="3">Sylhet</option>
+                            </select>
+                        </div>
                     </div>
-                    <div className="col-10 col-md-4">
-                        <h5 className="text-danger fw-bold">District:</h5>
-                        <select
-                            className="form-select"
-                            aria-label="Default select example"
+                    <div className="d-flex justify-content-center">
+                        <button
+                            onClick={handleSearch}
+                            className="btn btn-danger mt-3 px-4"
                         >
-                            <option selected>Select District</option>
-                            <option value="1">Dhaka</option>
-                            <option value="2">Chittagong</option>
-                            <option value="3">Comilla</option>
-                            <option value="3">Rajshahi</option>
-                            <option value="3">Khulna</option>
-                            <option value="3">Jessore</option>
-                            <option value="3">Sylhet</option>
-                        </select>
+                            Search
+                        </button>
                     </div>
-                </div>
-                <div className="d-flex justify-content-center">
-                    <button
-                        onClick={handleSearch}
-                        className="btn btn-danger mt-3 px-4"
-                    >
-                        Search
-                    </button>
-                </div>
+                </form>
             </section>
         </>
     );
