2151d0d69b81f4b333066b16790415a925e035d0
RIDOY-ZZAMAN <ridoyzzaman957@outlook.com>
2022-04-14 01:18:35 +0600

    Dashboard slighlty done

---
+++ src/page/Dashboards/SideBar/SideBar.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './SideBar.css';
+import logo from '../../../Assets/img/logo.png'
+
+const SideBar = () => {
+    const { pathname } = useLocation();
+    const firstPath = pathname.split("/dashboard/")[1];
+    return (
+        
+        <div className='sidebar'>
+            <img src={logo} alt="" />
+            <div className="nav-content">
+                <Link to="/dashboard/dashboarHome" className={firstPath === "dashboarHome" ? "active" : ""}>
+                    Dashboard
+                </Link>
+                <Link to="/dashboard/doctor" className={firstPath === "doctor" ? "active" : ""}>
+                    Doctor
+                </Link>
+                <Link to="/dashboard/admin" className={firstPath === "admin" ? "active" : ""}>
+                    Admin
+                </Link>
+            </div>
+        </div>
+
+    );
+};
+
+export default SideBar;
