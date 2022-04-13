dd1ff7d1955a04422b2f9c5efb2f777e5e50db00
Pranta07 <palpranta@gmail.com>
2022-04-13 17:08:28 +0600

    comments removed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import Doctors from "./components/Doctors/Doctors";
 import Footer from "./components/Footer/Footer";
 import Nav from "./components/Nav/Nav";
@@ -14,22 +13,14 @@ import CovidPortal from "./page/CovidPortal/CovidPortal";
 import FindDonors from "./page/FindDonors/FindDonors";
 import Home from "./page/Home/Home";
 import Profile from "./page/Profile/Profile";
+import "./App.css";
 
 function App() {
     return (
         <>
             <AuthProvider>
-                {/* mdmahim shariar */}
                 <Nav></Nav>
                 <Routes>
-                    {/* Mostofa Reza */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* Alamin */}
-
                     <Route path="/" element={<Home />}></Route>
                     <Route path="/signUp" element={<Registation />}></Route>
                     <Route path="/login" element={<Login />}></Route>
@@ -40,93 +31,14 @@ function App() {
                         path="/videoRoute"
                         element={<VideoChatRoute />}
                     ></Route>
-                    {/* md mahim shariar  */}
 
-                    {/* Suresh Pal Pranta */}
                     <Route
                         path="/CovidPortal"
                         element={<CovidPortal />}
                     ></Route>
                     <Route path="/FindDonors" element={<FindDonors />}></Route>
-
-                    {/* Suresh Pal Pranta */}
-
-                    {/* Mostofa Reza */}
-
-                    {/* Mostofa Reza */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* Alamin */}
-
-                    {/* md mahim shariar  */}
-
-                    {/* Suresh Pal Pranta */}
-
-                    {/* Suresh Pal Pranta */}
-
-                    {/* Mostofa Reza */}
-
-                    {/* Mostofa Reza */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* Alamin */}
-
-                    {/* Alamin */}
-
-                    <Route
-                        path="/videoRoute"
-                        element={<VideoChatRoute />}
-                    ></Route>
-                    {/* md mahim shariar  */}
-
-                    {/* Suresh Pal Pranta */}
-                    <Route
-                        path="/CovidPortal"
-                        element={<CovidPortal />}
-                    ></Route>
-                    <Route path="/FindDonors" element={<FindDonors />}></Route>
-
-                    {/* Suresh Pal Pranta */}
-
-                    {/* Mostofa Reza */}
-
-                    {/* Mostofa Reza */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* Alamin */}
-
-                    {/* md mahim shariar  */}
-
-                    {/* Suresh Pal Pranta */}
-
-                    {/* Suresh Pal Pranta */}
-
-                    {/* Mostofa Reza */}
-
-                    {/* Mostofa Reza */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* AKTARUZZAMAN RIDOY */}
-
-                    {/* Alamin */}
-
-                    {/* Alamin */}
-
-                    {/* Mahadi */}
 
                     <Route path="/doctors" element={<Doctors />}></Route>
-
-                    {/* Mahadi */}
                 </Routes>
                 <Footer></Footer>
             </AuthProvider>
