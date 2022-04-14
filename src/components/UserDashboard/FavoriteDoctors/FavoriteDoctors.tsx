66e341cb743b7c18af7669637e5b9e3a91b9b2dc
Pranta07 <palpranta@gmail.com>
2022-04-14 22:09:43 +0600

    favoriteDoctors done

--- src/components/UserDashboard/FavoriteDoctors/FavoriteDoctors.tsx
+++ src/components/UserDashboard/FavoriteDoctors/FavoriteDoctors.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import SingleDoctor from "../SingleDoctor/SingleDoctor";
 
 const FavoriteDoctors = () => {
-    return <div>Favorite</div>;
+    const [favoriteDoctors, setFavoriteDoctors] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [remove, setRemove] = useState(false);
+
+    useEffect(() => {
+        setLoading(true);
+        const favoriteList = localStorage.getItem("favdoc");
+        if (favoriteList) {
+            const authors = JSON.parse(favoriteList);
+            setFavoriteDoctors(authors);
+        }
+        setLoading(false);
+    }, [remove]);
+
+    return (
+        <Container>
+            <h2
+                className="fw-bold text-center"
+                style={{ color: "rgb(69, 142, 167)" }}
+            >
+                Favorite Doctors
+            </h2>
+            {favoriteDoctors.map((doctor) => (
+                <SingleDoctor
+                    key={doctor._id}
+                    doctor={doctor}
+                    remove={remove}
+                    setRemove={setRemove}
+                ></SingleDoctor>
+            ))}
+        </Container>
+    );
 };
 
 export default FavoriteDoctors;
