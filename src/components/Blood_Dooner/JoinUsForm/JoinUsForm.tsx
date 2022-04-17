46ba5c85d9492ece6240241e49c575cef0442cb5
Pranta07 <palpranta@gmail.com>
2022-04-18 05:59:33 +0600

    api url updated

--- src/components/Blood_Dooner/JoinUsForm/JoinUsForm.tsx
+++ src/components/Blood_Dooner/JoinUsForm/JoinUsForm.tsx
@@ -29,7 +29,7 @@ const JoinUsForm = (props: any) => {
     const onSubmit: SubmitHandler<IFormInputs> = (data) => {
         // console.log(data);
         // send data to server and store in database
-        fetch("http://localhost:5000/donor/add", {
+        fetch("https://immense-beyond-64415.herokuapp.com/donor/add", {
             method: "POST",
             headers: {
                 "content-type": "application/json",
