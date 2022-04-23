fb4802bde80247d465671c8b782174e7477f7003
Al Amin <alaminhossain2000and@gmail.com>
2022-04-23 21:05:54 +0600

    appointment backend is done

---
+++ src/page/Dashboards/AllAppointments/AllAppointments.tsx
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
+import AllAppointment from '../AllAppointment/AllAppointment';
+
+const AllAppointments = () => {
+    const [appointments,setAppointments]=useState([]);
+    useEffect(()=>{
+        fetch("http://localhost:5000/allAppointments")
+        .then(res=>res.json())
+        .then(data=>setAppointments(data))
+    },[])
+    return (
+        <div>
+            <Table>
+            <thead>
+    <tr>
+      <th>Appointment Id</th>
+      <th>Patient Name</th>
+      <th>Patient Email</th>
+      <th>Doctor Name</th>
+      <th>Doctor Specialize</th>
+      <th>Timeslot</th>
+      <th>Status</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+      {
+          appointments.map((appointment:any,id)=><AllAppointment appointment={appointment} key={id}></AllAppointment>)
+      }
+  </tbody>
+            </Table>
+        </div>
+    );
+};
+
+export default AllAppointments;
