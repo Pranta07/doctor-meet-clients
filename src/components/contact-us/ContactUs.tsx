9367fa355dbb37b61b622e983a69f4fc5b72072c
Pranta07 <palpranta@gmail.com>
2022-05-03 01:58:53 +0600

    ui updated

--- src/components/contact-us/ContactUs.tsx
+++ src/components/contact-us/ContactUs.tsx
@@ -3,7 +3,6 @@ import emailjs from "emailjs-com";
 import { Container } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
 import "./ContactUs.css";
-import { Icon } from "@iconify/react";
 import MapDirection from "../map-ditrection/MapDirection";
 import { styled } from "@mui/material/styles";
 import Page from "../Page";
@@ -165,7 +164,7 @@ const ContactUs = () => {
                         </div>
                     </div>
 
-                    <div className="row d-flex align-items-center pt-5 mt-5">
+                    <div className="row d-flex align-items-center py-5 my-5">
                         <div className="col-lg-6">
                             <img src={ContactImg} alt="" className="w-100" />
                         </div>
@@ -247,95 +246,6 @@ const ContactUs = () => {
                         </div>
                     </div>
                 </Container>
-
-                <div className="bg-style-for-contact">
-                    <div className="container">
-                        <div className="row g-3 p-5 my-auto d-flex align-items-center">
-                            <div className="col-lg-3 text-light">
-                                <div className="d-flex align-items-center">
-                                    <div className="contact-us-btn mx-3">
-                                        <span className="text-center">
-                                            {" "}
-                                            <Icon icon="cil:hospital" />{" "}
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <h6 className="fw-bold"> ADDRESS </h6>
-                                        <small>
-                                            <p className="m-0">
-                                                House No#1, 8 Satmasjid Road,
-                                                Dhaka 1207
-                                            </p>
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3">
-                                <div className="d-flex text-light align-items-center">
-                                    <div className="contact-us-btn mx-3">
-                                        <span className="text-center">
-                                            {" "}
-                                            <Icon icon="fluent:call-add-24-regular" />
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <h6 className="fw-bold"> CALL US </h6>
-                                        <small>
-                                            <p className="m-0">
-                                                +8809003439 <br />
-                                                +8809003440
-                                            </p>
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3">
-                                <div className="d-flex text-light align-items-center">
-                                    <div className="contact-us-btn-write mx-3">
-                                        <span className="text-center">
-                                            {" "}
-                                            <Icon icon="fontisto:prescription" />
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <h6 className="fw-bold">
-                                            {" "}
-                                            WRITE TO US{" "}
-                                        </h6>
-                                        <small>
-                                            <p className="m-0">
-                                                office@medicare.com
-                                                book@medicare.com
-                                            </p>
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3">
-                                <div className="d-flex text-light align-items-center">
-                                    <div className="contact-us-btn mx-3">
-                                        <span className="text-center">
-                                            {" "}
-                                            <Icon icon="iconoir:healthcare" />
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <h6 className="fw-bold">
-                                            {" "}
-                                            BOOK AN APPOINTMENT{" "}
-                                        </h6>
-                                        <small>
-                                            <p className="m-0">
-                                                Click here to book an
-                                                appointment at Medicare.
-                                            </p>
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </RootStyle>
         </Page>
     );
--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -50,7 +50,7 @@ const About = () => {
                     {" > "} <span> About Us </span>
                 </div>
 
-                <div className="container mx-auto text-left">
+                <div className="container mx-auto text-left mb-5 pb-5">
                     <div className="row">
                         <div className="col-12 col-lg-8 mt-5">
                             <div className="bg-white border rounded-3 p-4">
@@ -109,95 +109,6 @@ const About = () => {
                                     {category.toUpperCase()}
                                 </p>
                             ))}
-                        </div>
-                    </div>
-                </div>
-
-                <div className="bg-style-for-contact">
-                    <div className="container">
-                        <div className="row p-5 my-auto d-flex align-items-center">
-                            <div className="col-lg-3 text-light">
-                                <div className="d-flex align-items-center">
-                                    <div className="contact-us-btn mx-3">
-                                        <span className="text-center">
-                                            {" "}
-                                            <Icon icon="cil:hospital" />{" "}
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <h6 className="fw-bold"> ADDRESS </h6>
-                                        <small>
-                                            <p className="m-0">
-                                                House No#1, 8 Satmasjid Road,
-                                                Dhaka 1207
-                                            </p>
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3">
-                                <div className="d-flex text-light align-items-center">
-                                    <div className="contact-us-btn mx-3">
-                                        <span className="text-center">
-                                            {" "}
-                                            <Icon icon="fluent:call-add-24-regular" />
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <h6 className="fw-bold"> CALL US </h6>
-                                        <small>
-                                            <p className="m-0">
-                                                +8809003439 <br />
-                                                +8809003440
-                                            </p>
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3">
-                                <div className="d-flex text-light align-items-center">
-                                    <div className="contact-us-btn-write mx-3">
-                                        <span className="text-center">
-                                            {" "}
-                                            <Icon icon="fontisto:prescription" />
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <h6 className="fw-bold">
-                                            {" "}
-                                            WRITE TO US{" "}
-                                        </h6>
-                                        <small>
-                                            <p className="m-0">
-                                                office@medicare.com
-                                                book@medicare.com
-                                            </p>
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="col-lg-3">
-                                <div className="d-flex text-light align-items-center">
-                                    <div className="contact-us-btn mx-3">
-                                        <span className="text-center">
-                                            {" "}
-                                            <Icon icon="iconoir:healthcare" />
-                                        </span>
-                                    </div>
-                                    <div>
-                                        <h6 className="fw-bold">
-                                            {" "}
-                                            BOOK AN APPOINTMENT{" "}
-                                        </h6>
-                                        <small>
-                                            <p className="m-0">
-                                                Click here to book an
-                                                appointment at Medicare.
-                                            </p>
-                                        </small>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
