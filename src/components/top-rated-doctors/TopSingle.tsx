98b4a1b92cc9b5c5e51e39c6cf642a8571c5bcde
Pranta07 <palpranta@gmail.com>
2022-05-03 05:44:34 +0600

    ui changed

--- src/components/top-rated-doctors/TopSingle.tsx
+++ src/components/top-rated-doctors/TopSingle.tsx
@@ -18,22 +18,22 @@ const TopSingle = (props: any) => {
                     />
                 </div>
                 <div className="p-4">
-                    <h4 className="d-name">{name}</h4>
-                    <h5 className="d-spec">{specialist}</h5>
-                    <p>
+                    <h4 className="d-name m-0">{name}</h4>
+                    <h5 className="d-spec pb-1">{specialist}</h5>
+                    <p className="m-0">
                         <FontAwesomeIcon
                             className="icon"
                             icon={faLocationDot}
                         ></FontAwesomeIcon>
                         <span className="text-secondary">Bangladesh</span>
                     </p>
-                    <Rating
+                    {/* <Rating
                         name="read-only"
                         value={review}
                         readOnly
                         size="small"
                         precision={0.5}
-                    />
+                    /> */}
                 </div>
             </div>
         </div>
