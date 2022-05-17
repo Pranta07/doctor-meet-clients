688158f62ff5bce966ab7d317fb47d92097ea16c
Pranta07 <palpranta@gmail.com>
2022-05-17 20:31:30 +0600

    main - merged

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,14 +8,12 @@ import LogoOnlyLayout from "../layouts/LogoOnlyLayout";
 import LoadingScreen from "../components/LoadingScreen";
 import MainLayout from "../layouts/main/index";
 import UserReview from "../components/user-review/UserReview";
-<<<<<<< HEAD
 import AddArticle from "../components/add-article/AddArticle";
-=======
 import DiagnosticCenter from "../components/diagnostic-center/DiagnosticCenter";
-import { DiagnosicPay,  DiagnosticAppointmentForm } from "../components/diagnostic-center";
-
-
->>>>>>> 910bb914b20bfa9055802e8d1652a4d9a447e86d
+import {
+    DiagnosicPay,
+    DiagnosticAppointmentForm,
+} from "../components/diagnostic-center";
 
 // ----------------------------------------------------------------------
 
@@ -181,28 +179,24 @@ export default function Router() {
                         },
                         {
                             path: "Report-section",
-                            element: <ReportSection/>,
+                            element: <ReportSection />,
                         },
                         {
-                            path:"Report-status",
-                            element:<ReportStatus/>,
+                            path: "Report-status",
+                            element: <ReportStatus />,
                         },
                         {
-                            
                             path: "add-review",
                             element: <UserReview />,
                         },
                         {
-<<<<<<< HEAD
                             path: "add-article",
                             element: <AddArticle />,
                         },
-=======
-                            path:"my-diagnosises",
-                            element:<MyDiagnosises />,
-                        }
-                        
->>>>>>> 910bb914b20bfa9055802e8d1652a4d9a447e86d
+                        {
+                            path: "my-diagnosises",
+                            element: <MyDiagnosises />,
+                        },
                     ],
                 },
                 {
@@ -221,12 +215,12 @@ export default function Router() {
                         { path: "manage-donors", element: <ManageDonors /> },
                         {
                             path: "all-appointments",
-                            element: <AllAppointments />
+                            element: <AllAppointments />,
                         },
-                         {
-                             path: "all-diagnosis",
-                             element: <AllDiagnosis/>,
-                         }
+                        {
+                            path: "all-diagnosis",
+                            element: <AllDiagnosis />,
+                        },
                     ],
                 },
             ],
@@ -281,18 +275,16 @@ export default function Router() {
                 },
                 {
                     path: "diagnostic-center",
-                    element: <DiagnosticCenter/>,
+                    element: <DiagnosticCenter />,
                 },
                 {
                     path: "/diagnostic-appointment-form/:category/:id",
-                    element: <DiagnosticAppointmentForm/>,
+                    element: <DiagnosticAppointmentForm />,
                 },
                 {
-                    path:"/diagnostic-pay/:id",
-                    element:<DiagnosicPay />,
+                    path: "/diagnostic-pay/:id",
+                    element: <DiagnosicPay />,
                 },
-               
-                
             ],
         },
         // { path: "*", element: <Navigate to="/404" replace /> },
@@ -400,13 +392,19 @@ const PayAppointmentFee = Loadable(
     lazy(() => import("../components/appointment/PayAppointmentFee"))
 );
 const MyDiagnosises = Loadable(
-    lazy(() => import("../components/diagnostic-center/my-diagnosis/MyDiagnosises"))
+    lazy(
+        () =>
+            import("../components/diagnostic-center/my-diagnosis/MyDiagnosises")
+    )
 );
 const ReportStatus = Loadable(
-    lazy(() => import("../components/report-review-section/report-status-section/ReportStatus"))
+    lazy(
+        () =>
+            import(
+                "../components/report-review-section/report-status-section/ReportStatus"
+            )
+    )
 );
-
-
 
 // DASHBOARD
 
