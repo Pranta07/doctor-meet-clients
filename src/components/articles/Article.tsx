ebc837d495e90c705e5d467bbe8fcb8e1361ae3a
Pranta07 <palpranta@gmail.com>
2022-05-18 14:08:28 +0600

    slider added

--- src/components/articles/Article.tsx
+++ src/components/articles/Article.tsx
@@ -10,58 +10,54 @@ const Article = (props: { key: number; article: IArticle }) => {
     const { img, author, title } = props.article;
 
     return (
-        <div className="col-12 col-md-6 col-lg-4 my-3 d-flex justify-content-center">
-            <Card
-                style={{
-                    borderRadius: "8px",
-                    padding: "14px",
-                    display: "block",
-                }}
-            >
-                <div className="position-relative">
-                    <Card.Img
-                        variant="top"
-                        src={img}
-                        style={{ borderRadius: "8px" }}
-                    />
-                    <span
-                        className="px-4 py-2 fw-bold"
-                        style={{
-                            position: "absolute",
-                            bottom: "-15px",
-                            right: "10px",
-                            borderRadius: "18px",
-                            backgroundColor: "#00acb1",
-                            color: "white",
-                            fontSize: "12px",
-                        }}
-                    >
-                        1 MAY, 2022
-                    </span>
-                </div>
-                <Card.Body className="px-0 pt-4 pb-0">
-                    <p>
-                        <FontAwesomeIcon
-                            className="mx-1 icon"
-                            icon={faUserAlt}
-                        />
-                        <span className="text-secondary">{author}</span>
-                    </p>
-                    <h5
-                        style={{
-                            color: "#005963",
-                            fontWeight: "bold",
-                        }}
-                    >
-                        {title}
-                    </h5>
-                    <Button className="mt-2 mb-0">
-                        Read more{" "}
-                        <FontAwesomeIcon className="mx-1" icon={faArrowRight} />
-                    </Button>
-                </Card.Body>
-            </Card>
-        </div>
+        <Card
+            className="m-2 h-100"
+            style={{
+                borderRadius: "8px",
+                padding: "14px",
+                display: "block",
+            }}
+        >
+            <div className="position-relative">
+                <Card.Img
+                    variant="top"
+                    src={img}
+                    style={{ borderRadius: "8px" }}
+                />
+                <span
+                    className="px-4 py-2 fw-bold"
+                    style={{
+                        position: "absolute",
+                        bottom: "-15px",
+                        right: "10px",
+                        borderRadius: "18px",
+                        backgroundColor: "#00acb1",
+                        color: "white",
+                        fontSize: "12px",
+                    }}
+                >
+                    1 MAY, 2022
+                </span>
+            </div>
+            <Card.Body className="px-0 pt-4 pb-0">
+                <p>
+                    <FontAwesomeIcon className="mx-1 icon" icon={faUserAlt} />
+                    <span className="text-secondary">{author}</span>
+                </p>
+                <h5
+                    style={{
+                        color: "#005963",
+                        fontWeight: "bold",
+                    }}
+                >
+                    {title}
+                </h5>
+                <Button className="mt-2 mb-0">
+                    Read more{" "}
+                    <FontAwesomeIcon className="mx-1" icon={faArrowRight} />
+                </Button>
+            </Card.Body>
+        </Card>
     );
 };
 
