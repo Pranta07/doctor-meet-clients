a46875321a55cb15b4ac70f4a275a69388e63861
mhasancy <mhasancy@gmail.com>
2022-05-21 03:32:16 +0600

    all in one

--- src/redux/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import donorReducer from './donorSlice';
-export const store = configureStore({
-    reducer: {
-        donors: donorReducer,
-    },
-});
