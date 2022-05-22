779de0e2bbff596de4979b1435a6da4f49117ab0
Pranta07 <palpranta@gmail.com>
2022-05-23 04:04:43 +0600

    refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, Routes } from "react-router-dom";
 import AuthProvider from "./contexts/AuthProvider";
 import Router from "./routes/index";
 // theme
@@ -16,27 +15,24 @@ import { loadUser } from "./redux/actions/userAction";
 import store from "./redux/store";
 // ----------------------------------------------------------------------
 
-
-
 export default function App() {
-  const token = window.localStorage.getItem("token");
+    const token = window.localStorage.getItem("token");
 
-  useEffect(() => {
-
-      store.dispatch(loadUser());
-  }, [token]);
-  return (
-    <ThemeProvider>
-      <NotistackProvider>
-        <MotionLazyContainer>
-          <AuthProvider>
-            <ProgressBarStyle />
-            <ChartStyle />
-            <ScrollToTop />
-            <Router />
-          </AuthProvider>
-        </MotionLazyContainer>
-      </NotistackProvider>
-    </ThemeProvider>
-  );
+    useEffect(() => {
+        store.dispatch(loadUser());
+    }, [token]);
+    return (
+        <ThemeProvider>
+            <NotistackProvider>
+                <MotionLazyContainer>
+                    <AuthProvider>
+                        <ProgressBarStyle />
+                        <ChartStyle />
+                        <ScrollToTop />
+                        <Router />
+                    </AuthProvider>
+                </MotionLazyContainer>
+            </NotistackProvider>
+        </ThemeProvider>
+    );
 }
