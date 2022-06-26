25049f2c01842fea6b2e10325da11a596090887d
Pranta07 <palpranta@gmail.com>
2022-06-26 18:38:01 +0600

    deployed

--- src/layouts/dashboard/navbar/NavbarVertical.tsx
+++ src/layouts/dashboard/navbar/NavbarVertical.tsx
@@ -17,176 +17,177 @@ import Scrollbar from "../../../components/Scrollbar";
 import { NavSectionVertical } from "../../../components/nav-section/index";
 //
 import {
-  sidebarConfigAdmin,
-  sidebarConfigUser,
-  sidebarConfigModerator,
-  sidebarConfigDoctor,
+    sidebarConfigAdmin,
+    sidebarConfigUser,
+    sidebarConfigModerator,
+    sidebarConfigDoctor,
 } from "./NavConfig";
 import NavbarAccount from "./NavbarAccount";
 import CollapseButton from "./CollapseButton";
-import logo from "../../../assets/img/logo.png";
 import { useAppSelector } from "../../../redux/store";
 
 // ----------------------------------------------------------------------
 
 const RootStyle = styled("div")(({ theme }: any) => ({
-  [theme.breakpoints.up("lg")]: {
-    flexShrink: 0,
-    transition: theme.transitions.create("width", {
-      duration: theme.transitions.duration.shorter,
-    }),
-  },
+    [theme.breakpoints.up("lg")]: {
+        flexShrink: 0,
+        transition: theme.transitions.create("width", {
+            duration: theme.transitions.duration.shorter,
+        }),
+    },
 }));
 
 // ----------------------------------------------------------------------
 
 NavbarVertical.propTypes = {
-  isOpenSidebar: PropTypes.bool,
-  onCloseSidebar: PropTypes.func,
+    isOpenSidebar: PropTypes.bool,
+    onCloseSidebar: PropTypes.func,
 };
 
 export default function NavbarVertical({ isOpenSidebar, onCloseSidebar }: any) {
-  const { user }: any = useAppSelector((state) => state?.user);
-  console.log(user);
+    const { user }: any = useAppSelector((state) => state?.user);
+    console.log(user);
 
-  const theme = useTheme();
+    const theme = useTheme();
 
-  const { pathname } = useLocation();
+    const { pathname } = useLocation();
 
-  const isDesktop = useResponsive("up", "lg");
+    const isDesktop = useResponsive("up", "lg");
 
-  const {
-    isCollapse,
-    collapseClick,
-    collapseHover,
-    onToggleCollapse,
-    onHoverEnter,
-    onHoverLeave,
-  }: any = useCollapseDrawer();
+    const {
+        isCollapse,
+        collapseClick,
+        collapseHover,
+        onToggleCollapse,
+        onHoverEnter,
+        onHoverLeave,
+    }: any = useCollapseDrawer();
 
-  useEffect(() => {
-    if (isOpenSidebar) {
-      onCloseSidebar();
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [pathname]);
+    useEffect(() => {
+        if (isOpenSidebar) {
+            onCloseSidebar();
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [pathname]);
 
-  const renderContent = (
-    <Scrollbar
-      sx={{
-        height: 1,
-        "& .simplebar-content": {
-          height: 1,
-          display: "flex",
-          flexDirection: "column",
-        },
-      }}
-    >
-      <Stack
-        spacing={3}
-        sx={{
-          pt: 3,
-          pb: 2,
-          px: 2.5,
-          flexShrink: 0,
-          ...(isCollapse && { alignItems: "center" }),
-        }}
-      >
-        <Stack
-          direction="row"
-          alignItems="center"
-          justifyContent="space-between"
+    const renderContent = (
+        <Scrollbar
+            sx={{
+                height: 1,
+                "& .simplebar-content": {
+                    height: 1,
+                    display: "flex",
+                    flexDirection: "column",
+                },
+            }}
         >
-          <Logo />
+            <Stack
+                spacing={3}
+                sx={{
+                    pt: 3,
+                    pb: 2,
+                    px: 2.5,
+                    flexShrink: 0,
+                    ...(isCollapse && { alignItems: "center" }),
+                }}
+            >
+                <Stack
+                    direction="row"
+                    alignItems="center"
+                    justifyContent="space-between"
+                >
+                    <Logo />
 
-          {isDesktop && !isCollapse && (
-            <CollapseButton
-              onToggleCollapse={onToggleCollapse}
-              collapseClick={collapseClick}
-            />
-          )}
-        </Stack>
+                    {isDesktop && !isCollapse && (
+                        <CollapseButton
+                            onToggleCollapse={onToggleCollapse}
+                            collapseClick={collapseClick}
+                        />
+                    )}
+                </Stack>
 
-        <NavbarAccount isCollapse={isCollapse} />
-      </Stack>
-      {user?.role === "admin" ? (
-        <NavSectionVertical
-          navConfig={sidebarConfigAdmin}
-          isCollapse={isCollapse}
-        />
-      ) : user?.role === "modaretor" ? (
-        <NavSectionVertical
-          navConfig={sidebarConfigModerator}
-          isCollapse={isCollapse}
-        />
-      ) : user?.role === "doctor" ? (
-        <NavSectionVertical
-          navConfig={sidebarConfigDoctor}
-          isCollapse={isCollapse}
-        />
-      ) : (
-        <NavSectionVertical
-          navConfig={sidebarConfigUser}
-          isCollapse={isCollapse}
-        />
-      )}
+                <NavbarAccount isCollapse={isCollapse} />
+            </Stack>
+            {user?.role === "admin" ? (
+                <NavSectionVertical
+                    navConfig={sidebarConfigAdmin}
+                    isCollapse={isCollapse}
+                />
+            ) : user?.role === "modaretor" ? (
+                <NavSectionVertical
+                    navConfig={sidebarConfigModerator}
+                    isCollapse={isCollapse}
+                />
+            ) : user?.role === "doctor" ? (
+                <NavSectionVertical
+                    navConfig={sidebarConfigDoctor}
+                    isCollapse={isCollapse}
+                />
+            ) : (
+                <NavSectionVertical
+                    navConfig={sidebarConfigUser}
+                    isCollapse={isCollapse}
+                />
+            )}
 
-      <Box sx={{ flexGrow: 1 }} />
-      {!isCollapse && <Box p={2} />}
-    </Scrollbar>
-  );
+            <Box sx={{ flexGrow: 1 }} />
+            {!isCollapse && <Box p={2} />}
+        </Scrollbar>
+    );
 
-  return (
-    <RootStyle
-      sx={{
-        width: {
-          lg: isCollapse
-            ? NAVBAR.DASHBOARD_COLLAPSE_WIDTH
-            : NAVBAR.DASHBOARD_WIDTH,
-        },
-        ...(collapseClick && {
-          position: "absolute",
-        }),
-      }}
-    >
-      {!isDesktop && (
-        <Drawer
-          open={isOpenSidebar}
-          onClose={onCloseSidebar}
-          PaperProps={{ sx: { width: NAVBAR.DASHBOARD_WIDTH } }}
-        >
-          {renderContent}
-        </Drawer>
-      )}
-
-      {isDesktop && (
-        <Drawer
-          open
-          variant="persistent"
-          onMouseEnter={onHoverEnter}
-          onMouseLeave={onHoverLeave}
-          PaperProps={{
-            sx: {
-              width: NAVBAR.DASHBOARD_WIDTH,
-              borderRightStyle: "dashed",
-              bgcolor: "background.default",
-              transition: (theme) =>
-                theme.transitions.create("width", {
-                  duration: theme.transitions.duration.standard,
+    return (
+        <RootStyle
+            sx={{
+                width: {
+                    lg: isCollapse
+                        ? NAVBAR.DASHBOARD_COLLAPSE_WIDTH
+                        : NAVBAR.DASHBOARD_WIDTH,
+                },
+                ...(collapseClick && {
+                    position: "absolute",
                 }),
-              ...(isCollapse && {
-                width: NAVBAR.DASHBOARD_COLLAPSE_WIDTH,
-              }),
-              ...(collapseHover && {
-                ...cssStyles(theme).bgBlur(any),
-                boxShadow: (theme: any) => theme.customShadows.z24,
-              }),
-            },
-          }}
+            }}
         >
-          {renderContent}
-        </Drawer>
-      )}
-    </RootStyle>
-  );
+            {!isDesktop && (
+                <Drawer
+                    open={isOpenSidebar}
+                    onClose={onCloseSidebar}
+                    PaperProps={{ sx: { width: NAVBAR.DASHBOARD_WIDTH } }}
+                >
+                    {renderContent}
+                </Drawer>
+            )}
+
+            {isDesktop && (
+                <Drawer
+                    open
+                    variant="persistent"
+                    onMouseEnter={onHoverEnter}
+                    onMouseLeave={onHoverLeave}
+                    PaperProps={{
+                        sx: {
+                            width: NAVBAR.DASHBOARD_WIDTH,
+                            borderRightStyle: "dashed",
+                            bgcolor: "background.default",
+                            transition: (theme) =>
+                                theme.transitions.create("width", {
+                                    duration:
+                                        theme.transitions.duration.standard,
+                                }),
+                            ...(isCollapse && {
+                                width: NAVBAR.DASHBOARD_COLLAPSE_WIDTH,
+                            }),
+                            ...(collapseHover && {
+                                ...cssStyles(theme).bgBlur(any),
+                                boxShadow: (theme: any) =>
+                                    theme.customShadows.z24,
+                            }),
+                        },
+                    }}
+                >
+                    {renderContent}
+                </Drawer>
+            )}
+        </RootStyle>
+    );
 }
--- src/pages/security/login/Login.tsx
+++ src/pages/security/login/Login.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef } from "react";
 import { useSnackbar } from "notistack";
 import {
-  NavLink,
-  useLocation,
-  useNavigate,
-  unstable_HistoryRouter as HistoryRouter,
+    NavLink,
+    useLocation,
+    useNavigate,
+    unstable_HistoryRouter as HistoryRouter,
 } from "react-router-dom";
 
 import { useAppDispatch, useAppSelector } from "../../../redux/store";
@@ -17,118 +17,124 @@ import { Button, TextField } from "@mui/material";
 const history: any = createBrowserHistory({ window });
 
 const Login = () => {
-  const { user }: any = useAppSelector((state) => state.user);
-  const { enqueueSnackbar } = useSnackbar();
-  useEffect(() => {}, [user]);
-  const dispatch = useAppDispatch();
-  const navigate = useNavigate();
-  const mail = useRef<HTMLInputElement>(null!);
-  const pass = useRef<HTMLInputElement>(null!);
-  const handelSubmit = (e: any): void => {
-    e.preventDefault();
-    const email: string = mail.current?.value;
-    const password: string = pass.current?.value;
-    // signUsingEmail(mailE, passE);
-    dispatch(login(email, password));
-    if (user.success) {
-      navigate("/");
-      dispatch(clearErrors());
-      enqueueSnackbar("logged in successfully!");
-      if (history.location.pathname === "/signUp") {
-      } else {
-        navigate(history.back());
-      }
-    } else if (user.error) {
-      enqueueSnackbar(user.error);
-      dispatch(clearErrors());
-    }
-  };
-  return (
-    <div className="container my-5">
-      <div className="row py-5 my-5 ">
-        <div className="col-lg-6">
-          <img className="img-fluid" src={undrow_login} alt="" />
-        </div>
-        <div className="col-lg-6 my-auto py-5">
-          <div
-            style={{
-              width: "100%",
-            }}
-            className="container"
-          >
-            <h3
-              style={{
-                color: "#0074ff",
-              }}
-            >
-              {" "}
-              Sign In{" "}
-            </h3>
-            <hr
-              style={{
-                width: "100px",
-                border: `1px solid rgb(0 116 255)`,
-                boxShadow: "rgb(99 99 99 / 20%) 0px 2px 8px 0px",
-                margin: " 20px auto !important",
-                borderRadius: "10px",
-              }}
-            />
-            <form onSubmit={handelSubmit}>
-              <TextField
-                id="outlined-basic"
-                label="Email"
-                variant="outlined"
-                fullWidth
-                inputRef={mail}
-                required
-                type="email"
-                sx={{
-                  marginY: "10px",
-                }}
-              />
-              <TextField
-                id="outlined-basic"
-                label="Password"
-                variant="outlined"
-                fullWidth
-                required
-                inputRef={pass}
-                type="password"
-                sx={{
-                  marginY: "10px",
-                }}
-              />
-              <NavLink to="/password/forgot">
-                <Button variant="text" style={{ color: "rgb(0 116 255)" }}>
-                  {" "}
-                  Forget Password?{" "}
-                </Button>{" "}
-                <br />
-              </NavLink>
-              <Button
-                type="submit"
-                sx={{ marginY: "10px" }}
-                variant="contained"
-              >
-                Login
-              </Button>
+    const { user }: any = useAppSelector((state) => state.user);
+    const { enqueueSnackbar } = useSnackbar();
+    useEffect(() => {}, [user]);
+    const dispatch = useAppDispatch();
+    const navigate = useNavigate();
+    const mail = useRef<HTMLInputElement>(null!);
+    const pass = useRef<HTMLInputElement>(null!);
+    const handelSubmit = (e: any): void => {
+        e.preventDefault();
+        const email: string = mail.current?.value;
+        const password: string = pass.current?.value;
+        // signUsingEmail(mailE, passE);
+        dispatch(login(email, password));
+        if (user.success) {
+            navigate("/");
+            dispatch(clearErrors());
+            enqueueSnackbar("logged in successfully!");
+            if (history.location.pathname === "/signUp") {
+            } else {
+                navigate(history.back());
+            }
+        } else if (user.error) {
+            enqueueSnackbar(user.error);
+            dispatch(clearErrors());
+        }
+    };
+    return (
+        <div className="container my-5">
+            <div className="row py-5 my-5 ">
+                <div className="col-lg-6">
+                    <img className="img-fluid" src={undrow_login} alt="" />
+                </div>
+                <div className="col-lg-6 my-auto py-5">
+                    <div
+                        style={{
+                            width: "100%",
+                        }}
+                        className="container"
+                    >
+                        <h3
+                            style={{
+                                color: "#0074ff",
+                            }}
+                        >
+                            {" "}
+                            Sign In{" "}
+                        </h3>
+                        <hr
+                            style={{
+                                width: "100px",
+                                border: `1px solid rgb(0 116 255)`,
+                                boxShadow:
+                                    "rgb(99 99 99 / 20%) 0px 2px 8px 0px",
+                                margin: " 20px auto !important",
+                                borderRadius: "10px",
+                            }}
+                        />
+                        <form onSubmit={handelSubmit}>
+                            <TextField
+                                id="outlined-basic"
+                                label="Email"
+                                variant="outlined"
+                                fullWidth
+                                inputRef={mail}
+                                required
+                                type="email"
+                                sx={{
+                                    marginY: "10px",
+                                }}
+                            />
+                            <TextField
+                                id="outlined-basic"
+                                label="Password"
+                                variant="outlined"
+                                fullWidth
+                                required
+                                inputRef={pass}
+                                type="password"
+                                sx={{
+                                    marginY: "10px",
+                                }}
+                            />
+                            <NavLink to="/password/forgot">
+                                <Button
+                                    variant="text"
+                                    style={{ color: "rgb(0 116 255)" }}
+                                >
+                                    {" "}
+                                    Forget Password?{" "}
+                                </Button>{" "}
+                                <br />
+                            </NavLink>
+                            <Button
+                                type="submit"
+                                sx={{ marginY: "10px" }}
+                                variant="contained"
+                            >
+                                Login
+                            </Button>
 
-              <p>
-                {" "}
-                <span style={{ fontWeight: "600", color: "gray" }}>
-                  {" "}
-                  Not registered yet?
-                </span>
-                <NavLink to="/signUp">
-                  <Button> Create an Account </Button>
-                </NavLink>
-              </p>
-            </form>
-          </div>
+                            <p>
+                                {" "}
+                                <span
+                                    style={{ fontWeight: "600", color: "gray" }}
+                                >
+                                    {" "}
+                                    Not registered yet?
+                                </span>
+                                <NavLink to="/signUp">
+                                    <Button> Create an Account </Button>
+                                </NavLink>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default Login;
